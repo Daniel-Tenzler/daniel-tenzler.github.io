fb4091993e8e52b9a207557bdba6eeb7fcd896c6
dan_tenz0119 <d.tebzler@campus.tu-berlin.de>
2025-05-29 21:32:43 +0200

    revert all changes as remix export does not work

--- app/routes/_.jsx
+++
@@ -1,9 +0,0 @@
-export default function NotFound() {
-    return (
-      <div>
-        <h1>404 – Not Found</h1>
-        <p>We couldn’t find the page you&apos;re looking for.</p>
-      </div>
-    );
-  }
-  
--- app/routes/index.jsx
+++
@@ -1,8 +0,0 @@
-export default function Index() {
-  return (
-    <main>
-      <h1>Hello, world!</h1>
-      <p>This is your minimal Remix app in JavaScript.</p>
-    </main>
-  );
-}
