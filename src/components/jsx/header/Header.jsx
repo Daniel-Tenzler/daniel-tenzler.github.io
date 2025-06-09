89db72934ef9019fd6f3705d5d67c0b5d9e38c33
dan_tenz0119 <d.tebzler@campus.tu-berlin.de>
2025-06-09 10:55:15 +0200

    adjust layout, fix mobile header

--- src/components/jsx/header/Header.jsx
+++ src/components/jsx/header/Header.jsx
@@ -88,8 +88,55 @@ const MenuIcon = styled.svg`
   width: 1.5rem;
 `;
 
+const MobileMenu = styled.div`
+  display: ${props => props.isOpen ? 'block' : 'none'};
+  position: absolute;
+  top: 100%;
+  left: 0;
+  right: 0;
+  background-color: ${COLORS.white};
+  border-bottom: 1px solid ${getRgbaColor(COLORS.grayLight)};
+  padding: 1rem;
+  box-shadow: 0 4px 6px -1px ${getRgbaColor(COLORS.black, 0.1)};
+
+  @media (min-width: 640px) {
+    display: none;
+  }
+`;
+
+const MobileNavLink = styled.a`
+  display: block;
+  color: ${COLORS.black};
+  padding: 0.75rem 1rem;
+  border-radius: 0.375rem;
+  font-size: 0.875rem;
+  font-weight: 500;
+  text-decoration: none;
+  transition: all 0.2s ease;
+
+  &:hover {
+    color: ${COLORS.gray};
+    background-color: ${getRgbaColor(COLORS.grayLight)};
+  }
+`;
+
 export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMobileMenuOpen: false
+    };
+  }
+
+  toggleMobileMenu = () => {
+    this.setState(prevState => ({
+      isMobileMenuOpen: !prevState.isMobileMenuOpen
+    }));
+  };
+
   render() {
+    const { isMobileMenuOpen } = this.state;
+    
     return (
       <StyledHeader>
         <Nav>
@@ -106,9 +153,10 @@ export default class Header extends React.Component {
             <MobileMenuButton
               type="button"
               aria-controls="mobile-menu"
-              aria-expanded="false"
+              aria-expanded={isMobileMenuOpen}
+              onClick={this.toggleMobileMenu}
             >
-              <span className="sr-only">Open main menu</span>
+              <span className="sr-only"/>
               <MenuIcon
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -120,11 +168,20 @@ export default class Header extends React.Component {
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth={2}
-                  d="M4 6h16M4 12h16M4 18h16"
+                  d={isMobileMenuOpen 
+                    ? "M6 18L18 6M6 6l12 12" 
+                    : "M4 6h16M4 12h16M4 18h16"
+                  }
                 />
               </MenuIcon>
             </MobileMenuButton>
           </NavContainer>
+          <MobileMenu isOpen={isMobileMenuOpen} id="mobile-menu">
+            <MobileNavLink href="/" onClick={this.toggleMobileMenu}>Home</MobileNavLink>
+            <MobileNavLink href="/portfolio" onClick={this.toggleMobileMenu}>Portfolio</MobileNavLink>
+            <MobileNavLink href="/blog" onClick={this.toggleMobileMenu}>Blog</MobileNavLink>
+            <MobileNavLink href="/contact" onClick={this.toggleMobileMenu}>Contact</MobileNavLink>
+          </MobileMenu>
         </Nav>
       </StyledHeader>
     );
